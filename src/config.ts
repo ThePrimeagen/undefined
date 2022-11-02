e9f16dba84fd6672c46a8322f37672ca65f90606
mpaulson <the.primeagen@gmail.com>
2022-11-02 12:51:20 -0600

    feat: config with tsconfig options around how to unionize

--- src/config.ts
+++ src/config.ts
@@ -1,25 +1,40 @@
+import fs from "fs";
 import yargs from "yargs";
-
 
 type PartialConfig = {
     nameBase?: string;
     enums?: string;
     file?: string | "stdin";
     unionCount?: number;
+    config: string;
 }
+
+export type TSConfig = {
+    unions: {[key: string]: string[]},
+    names: {[key: string]: string[]},
+};
 
 export type Config = {
     nameBase: string;
     enums: string[];
     file: string | "stdin";
     unionCount: number;
+    config: TSConfig;
 }
 
 // TODO: Normalize i use command-line-arguments fro this, tried yarg, need
 // to take more time to figure this out.
 const BASE_NAME = "BaseName";
 const UNION_COUNT = 4;
+const CONFIG = {
+    unions: {},
+    names: {},
+};
+
+// TODO: THIS IS MY LEAST FAVORITE THING I HAVE EVER DONE AND CLEARLY I DID NOT
+// RTFM
 export async function getConfig(): Promise<Config> {
+    // @ts-ignore
     const args = await yargs(process.argv).argv as PartialConfig;
 
     if ("enums" in args) {
@@ -42,6 +57,20 @@ export async function getConfig(): Promise<Config> {
         args.unionCount = UNION_COUNT;
     }
 
+    if (("config" in args)) {
+        const configFile = args.config;
+        try {
+            const config = JSON.parse(fs.readFileSync(configFile).toString());
+            args.config = config;
+        } catch (e) {
+            // @ts-ignore
+            throw new Error(`error'd while loading config. ${args.config} ${e.message}`);
+        }
+    } else {
+        // @ts-ignore
+        args.config = CONFIG;
+    }
+
     if (!("file" in args)) {
         if (process.argv[2].length < 3) {
             throw new Error("please provide a file either as the first positional argument or --file");
@@ -50,6 +79,8 @@ export async function getConfig(): Promise<Config> {
         return {
             nameBase: args.nameBase as string,
             unionCount: args.unionCount as number,
+            // @ts-ignore
+            config: args.config as TSConfig,
             enums: args.enums as unknown as string[],
             file: String(process.argv[2]),
         };
