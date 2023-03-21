94af336255ea0cd78619c1c6b11174a9025dc08c
mpaulson <mpaulson@netflix.com>
2023-03-21 10:02:49 -0600

    fix: file read

--- src/file.ts
+++ src/file.ts
@@ -1,12 +1,14 @@
+import fs from "fs";
 import readline from "readline";
-const rl = readline.createInterface({
-    input: process.stdin,
-});
 
 export default async function getData<T extends object>(file: string): Promise<T[]> {
     if (!file) {
         throw new Error("requires a file to read data from");
     }
+
+    const rl = readline.createInterface({
+        input: file === "stdin" ? process.stdin : fs.createReadStream(file),
+    });
 
     return new Promise(res => {
         const data: T[] = [];
